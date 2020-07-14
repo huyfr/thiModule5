b3517e0df3201d5207a70e5c75e97bd494ddeede
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-14 09:50:31 +0700

    completed component delete-book

--- src/app/edit-book/edit-book.component.ts
+++ src/app/edit-book/edit-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Book} from "../../book";
+import {BookService} from "../book.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit-book',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBookComponent implements OnInit {
 
-  constructor() { }
+  editForm: FormGroup;
+  currentBook: Book;
+  submitted = false;
+
+  constructor(private bookService: BookService,
+              private formBuilder: FormBuilder,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.editForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      description: ['']
+    });
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.bookService.findById(id).subscribe(
+      next => {
+        this.currentBook = next;
+        this.editForm.patchValue(this.currentBook);
+      }, error => {
+        console.log(error);
+        this.currentBook = null;
+      }
+    );
+  }
+
+  onSubmit(): void {
+    if (this.editForm.valid) {
+      const {value} = this.editForm;
+      const data = {
+        ...this.currentBook,
+        ...value
+      };
+      this.bookService.update(data).subscribe(
+        next => {
+          this.submitted = true;
+          this.router.navigateByUrl('');
+        }, error => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  get rfc() {
+    return this.editForm.controls;
+  }
+
+  backToIndex(): void {
+    this.submitted = false;
+    this.editForm.reset();
+    this.router.navigateByUrl('');
+
   }
 
 }
