d56f33d83f439d42e48156790ae10379e6d34afc
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-14 10:01:31 +0700

    completed component delete-book

--- src/app/delete-book/delete-book.component.ts
+++ src/app/delete-book/delete-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../book";
+import {ActivatedRoute, Router} from "@angular/router";
+import {BookService} from "../book.service";
 
 @Component({
   selector: 'app-delete-book',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteBookComponent implements OnInit {
 
-  constructor() { }
+  currentTodo: Book;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private bookService: BookService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.bookService.findById(id).subscribe(
+      next => {
+        this.currentTodo = next;
+      }, error => {
+        console.log(error);
+        this.currentTodo = null;
+      }
+    )
+  }
+
+  onDelete(): void {
+        let verify = confirm("Bạn có chắc chắn muốn xóa?");
+        if (verify) {
+          this.bookService.delete(this.currentTodo.id).subscribe( todo => {todo.id !== this.currentTodo.id});
+          this.submitted = true;
+          this.router.navigateByUrl('');
+        } else {
+          this.submitted = false;
+        }
+  }
+
+  backToIndex(): void {
+    this.submitted = false;
+    this.router.navigateByUrl('');
+  }
 }
