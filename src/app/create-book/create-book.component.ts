895dbaac5f740abea5d32a3487fa3789b75b3e44
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-14 09:43:12 +0700

    completed component create-book

--- src/app/create-book/create-book.component.ts
+++ src/app/create-book/create-book.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BookService} from "../book.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-book',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBookComponent implements OnInit {
 
-  constructor() { }
+  createForm: FormGroup;
+  submitted = false;
+
+  constructor(private bookService: BookService,
+              private formBuilder: FormBuilder,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.createForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      description: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.createForm.valid) {
+      const value = this.createForm.getRawValue();
+      console.log(value);
+      this.bookService.save(value).subscribe(error => {
+        console.log(error)
+      });
+      this.submitted = true;
+      this.router.navigateByUrl('');
+    }
+  }
+
+  backToIndex(): void {
+    this.submitted = false
+    this.createForm.reset();
+    this.router.navigateByUrl('');
+  }
+
+  get rfc() {
+    return this.createForm.controls;
   }
 
 }
