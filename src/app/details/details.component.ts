9163a02e9c7d6963707e34e5571ae5eb4afa7c42
huyfr <61008601+huyfr@users.noreply.github.com>
2020-07-14 09:22:06 +0700

    completed component details-book

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from "../../book";
+import {BookService} from "../book.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  currentBook: Book;
+
+  constructor(private bookService: BookService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.bookService.findById(id).subscribe(
+      next => {
+        this.currentBook = next;
+      }, error => {
+        console.log(error);
+        this.currentBook = null;
+      }
+    )
+  }
+
+  backToIndex(): void {
+    this.router.navigateByUrl('');
   }
 
 }
